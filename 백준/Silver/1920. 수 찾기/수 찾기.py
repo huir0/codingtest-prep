acb848f9ff77349cb22ebf98cd2a3f29e3e6bb3a
huir0 <128926390+huir0@users.noreply.github.com>
2023-04-13 19:44:05 +0900

    [Silver IV] Title: 수 찾기, Time: 412 ms, Memory: 47276 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,27 @@
+import sys
+
+n = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
+
+m = int(sys.stdin.readline())
+b = list(map(int, sys.stdin.readline().split()))
+
+a.sort()            
+
+def search(array, target, start, end):
+    while start <= end:
+        mid = (start+end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+for i in range(m):
+    result = search(a, b[i], 0, n-1)
+    if result is not None:
+        print(1)
+    else:
+        print(0)
