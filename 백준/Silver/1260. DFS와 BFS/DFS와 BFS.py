7f46850bdee0008568548652a80208e8ba523e0e
huir0 <128926390+huir0@users.noreply.github.com>
2023-04-24 20:46:03 +0900

    [Silver II] Title: DFS와 BFS, Time: 76 ms, Memory: 34184 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -0,0 +1,37 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+def dfs(edges, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for e in edges[v]:
+        if not visited[e]:
+            dfs(edges, e, visited)
+
+def bfs(edges, v, visited):
+    queue = deque([v])
+    visited[v] = True
+    while queue:
+        now = queue.popleft()
+        print(now, end=' ')
+        for e in edges[now]:
+            if not visited[e]:
+                queue.append(e)
+                visited[e] = True
+
+n, m, v = map(int, input().split())
+edges = [[] for _ in range(n + 1)]
+for _ in range(m):
+    a, b = map(int, input().split())
+    edges[a].append(b)
+    edges[b].append(a)
+for i in range(1, n + 1):
+    edges[i].sort()
+visited = [False] * (n + 1)
+dfs(edges, v, visited)
+print()
+visited = [False] * (n + 1)
+bfs(edges, v, visited)
+print()
