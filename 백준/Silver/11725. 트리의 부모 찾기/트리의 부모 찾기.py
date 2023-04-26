0313ce8b5a93cbcdc7c6590d57663c2b4976d7a1
huir0 <128926390+huir0@users.noreply.github.com>
2023-04-26 20:45:01 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 364 ms, Memory: 65060 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,25 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+input = sys.stdin.readline
+
+n = int(input())
+
+graph = [[] for _ in range(n+1)]
+parents = [0 for _ in range(n+1)]
+
+for i in range(1,n):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def dfs(start):
+    for i in graph[start]:
+        if parents[i] == 0:
+            parents[i] = start
+            dfs(i)
+
+dfs(1)
+
+for i in range(2, n+1):
+    print(parents[i])
