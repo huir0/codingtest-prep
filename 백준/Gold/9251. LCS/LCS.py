c7788a039ef385f6c58a363d7762154080107720
huir0 <128926390+huir0@users.noreply.github.com>
2023-04-28 21:49:29 +0900

    [Gold V] Title: LCS, Time: 596 ms, Memory: 56556 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+a = str(input().strip())
+b = str(input())
+
+ans = [[0 for _ in range(len(b)+1)] for _ in range(len(a)+1)]
+for i in range(1,len(a)+1):
+    for j in range(1, len(b)+1):
+        if a[i-1] == b[j-1]:
+            ans[i][j] = ans[i-1][j-1] + 1
+        else:
+            ans[i][j] = max(ans[i][j-1], ans[i-1][j])
+print(ans[-1][-1])
