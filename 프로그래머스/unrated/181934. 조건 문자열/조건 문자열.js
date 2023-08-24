3c166f505f569a0be9d8b9d2f1c1b24084d13a2c
huir0 <128926390+huir0@users.noreply.github.com>
2023-08-24 21:09:09 +0900

    [unrated] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,22 @@
+function solution(ineq, eq, n, m) {
+    var answer = 0;
+    if (ineq === ">") {
+        if (n > m) {
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+    } else {
+        if (n < m) {
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+    }
+    if (eq === "=") {
+        if (n === m) {
+            answer = 1;
+        }
+    }
+    return answer;
+}
