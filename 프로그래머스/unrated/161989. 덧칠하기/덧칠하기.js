a229af5908362df0b75aeff0ec82aac30b9e3399
huir0 <128926390+huir0@users.noreply.github.com>
2023-09-01 20:20:06 +0900

    [unrated] Title: 덧칠하기, Time: 2.58 ms, Memory: 38.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.js
@@ -0,0 +1,11 @@
+function solution(n, m, section) {
+    var answer = 1;
+    let paint = section[0];
+    for (let i = 1; i < section.length; i++) {
+        if (section[i] - paint + 1 > m) {
+            answer++;
+            paint = section[i];
+        }
+    }
+    return answer;
+}
