1b175733d8ce960798182a47cc9ac297b9247972
huir0 <128926390+huir0@users.noreply.github.com>
2023-09-01 19:30:29 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,14 @@
+function solution(number, k) {
+    var answer = '';
+    let stack = [];
+    for (let i = 0; i < number.length; i++) {
+        while (k > 0 && stack[stack.length - 1] < number[i]) {
+            stack.pop();
+            k--;
+        }
+        stack.push(number[i]);
+    }
+    stack.splice(stack.length - k, k);
+    answer = stack.join('');
+    return answer;
+}
