cf56231bb2806711ddd7b5828fd2f59820f8b252
huir0 <128926390+huir0@users.noreply.github.com>
2023-09-04 16:44:45 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,18 @@
+function solution(strings, n) {
+    var answer = [];
+    strings.sort((a,b) => {
+        if (a[n] < b[n]) {
+            return -1;
+        } else if (a[n] > b[n]) {
+            return 1;
+        } else {
+            if (a < b) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+    })
+    answer = strings;
+    return answer;
+}
