b55c73a492ad47f19564cf8a3fd71d1502ed2d8c
huir0 <128926390+huir0@users.noreply.github.com>
2023-09-26 13:26:20 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 111.15 ms, Memory: 57.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,11 @@
+function solution(elements) {
+    let set = new Set();
+    for (let i = 0; i < elements.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < elements.length; j++) {
+            sum += elements[(i+j) % elements.length];
+            set.add(sum);
+        }
+    }
+    return set.size;
+}
