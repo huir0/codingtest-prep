4678019de6e8e4a85d17ef8d3645614686049f06
huir0 <128926390+huir0@users.noreply.github.com>
2023-10-17 00:53:30 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,5 @@
+function solution(a, b, c) {
+    if (a !== b && b !== c && c !== a) return a+b+c;
+    else if (a === b && b === c) return (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
+    else return (a+b+c) * (a*a + b*b + c*c);
+}
