904bba6c980422781630299f8000f20ba0e7102a
huir0 <128926390+huir0@users.noreply.github.com>
2023-10-28 01:22:38 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,3 @@
+function solution(my_string, num1, num2) {
+    return my_string.substring(0,num1) + my_string[num2] + my_string.substring(num1 + 1, num2) + my_string[num1] + my_string.substring(num2 + 1);
+}
